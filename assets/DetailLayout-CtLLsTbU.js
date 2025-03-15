4793e56c961df0cafa4dc2e59321bc6a00cbc72e
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2025-03-15 02:17:30 +0200

    Updates

---
+++ assets/DetailLayout-CtLLsTbU.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-D7mWZ3uM.js";import"./pagination-BlIMZx3U.js";function m(){return t.jsx("div",{className:"md:mt-16 mt-24 pb-10",children:t.jsx(s,{})})}export{m as default};
