151a296b04a7858625f85a27632b866e96d93008
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2025-03-15 02:33:04 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/Movie-App">
+    <BrowserRouter basename="/Movie-App/">
       <div className="relative">
         {/* Navbar */}
         <Navbar />
